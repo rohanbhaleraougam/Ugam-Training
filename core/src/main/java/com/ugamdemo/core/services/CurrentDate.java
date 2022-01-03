3d25c2d7757492184692a684d6129ca2a778b92a
rohanbhaleraougam <85493544+rohanbhaleraougam@users.noreply.github.com>
2022-01-03 14:10:13 +0530

    Preprocessor Added
    Preprocessor Added

---
+++ core/src/main/java/com/ugamdemo/core/services/CurrentDate.java
@@ -0,0 +1,5 @@
+package com.ugamdemo.core.services;
+
+public interface CurrentDate {
+    void UpdateDate(String path);
+}
---
+++ core/src/main/java/com/ugamdemo/core/services/impl/CurrentDateImpl.java
@@ -0,0 +1,41 @@
+package com.ugamdemo.core.services.impl;
+
+
+import com.day.cq.commons.date.DateUtil;
+import com.ugamdemo.core.services.CurrentDate;
+import com.ugamdemo.core.utils.ResolverUtils;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.Node;
+import javax.jcr.Session;
+import java.util.Calendar;
+
+@Component(service = CurrentDate.class,immediate = true)
+public class CurrentDateImpl implements  CurrentDate{
+
+    private static final Logger LOG = LoggerFactory.getLogger(CurrentDateImpl.class);
+
+    @Reference
+    ResourceResolverFactory resourceResolverFactory;
+
+    @Override
+    public void UpdateDate(String path) {
+        try{
+            ResourceResolver serviceResourceResolver = ResolverUtils.newResolver(resourceResolverFactory);
+            Session session = serviceResourceResolver.adaptTo(Session.class);
+            Resource resource = serviceResourceResolver.getResource(path);
+            Node node = resource.adaptTo(Node.class);
+            node.setProperty("time" , DateUtil.parseISO8601(DateUtil.getISO8601Date(Calendar.getInstance())));
+            session.save();
+            session.logout();
+        } catch (Exception e) {
+            LOG.info(e.getMessage());
+        }
+    }
+}
