282bdbc309e8c161f30ac2820cc53bef43981187
rohanbhaleraougam <85493544+rohanbhaleraougam@users.noreply.github.com>
2022-01-04 15:58:00 +0530

    Added CAConfiguration
    Two pages English and German

---
+++ core/src/main/java/com/ugamdemo/core/config/UgamCAconfig.java
@@ -0,0 +1,18 @@
+package com.ugamdemo.core.config;
+
+import org.apache.sling.caconfig.annotation.Configuration;
+import org.apache.sling.caconfig.annotation.Property;
+
+@Configuration(label="",description = "")
+public @interface UgamCAconfig {
+
+    @Property(label = "siteCountry",
+            description = "Country Name")
+    String siteCountry() default "us";
+    @Property(label = "siteAdmin",
+            description = "Site Admin")
+    String siteAdmin() default "ugam";
+    @Property(label = "siteSection",
+            description = "Site Section")
+    String siteSection() default "Site Section";
+}
---
+++ core/src/main/java/com/ugamdemo/core/models/Caconfig.java
@@ -0,0 +1,7 @@
+package com.ugamdemo.core.models;
+
+public interface Caconfig {
+    public String getSiteCountry();
+    public String getSiteAdmin();
+    public String getSiteSection();
+}
